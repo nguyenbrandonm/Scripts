a215b53f73812ee3d0901e3003cd31af3e621ded
Brandon Nguyen <nguyenbrandonm@gmail.com>
2026-01-11 22:34:34 -0600

    Update and rename simple-passwords.sh to common-passwords.sh

--- simple-passwords.sh
+++
@@ -1,3 +0,0 @@
-# Generates common seasonal password patterns for testing (e.g., Spring2025, Winter24!, Password1):
-
-for x in 1 24 25 26; do printf '%s\n' {Password,Welcome,Spring,Summer,Fall,Autumn,Winter}"$x"{,\!} | tee -a passwords.txt; done
